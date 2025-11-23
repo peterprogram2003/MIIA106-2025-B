f6eb1abecb807bb6b496c9d8b290d3c5a29cc0c9
peter <6811860019@mut.ac.th>
2025-11-23 16:58:29 +0700

    การทดลองครั้งที่2

---
+++ LAB02/TEST01/TEST01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    std::cout << "-Name Watchara Niponram\n";
+    std::cout << "-Course: MIIA0106\n";
+    std::cout << "-Email. 6811860019@mut.ac.th\n";
+    return 0;
+}
